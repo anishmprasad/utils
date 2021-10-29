17c1fff57c4edf92a429dfa96fd0793f3ddc8ac3
Anish M Prasad <Anish.m.prasad@gmail.com>
2021-10-29 16:22:14 +0530

    Create internet-speed.js

---
+++ internet-speed.js
@@ -0,0 +1,47 @@
+/**
+    A small Util to calulate internet speed by using websocket 
+    powered by measurementlab.net
+*/
+
+import ndt7 from '@m-lab/ndt7';
+
+ndt7.test(
+			{
+				userAcceptedDataPolicy: true,
+				uploadworkerfile: "/ndt7-upload-worker.min.js",   // please check utils root folder
+				downloadworkerfile: "/ndt7-download-worker.min.js" // please check utils root folder
+			},
+			{
+			  serverChosen: function (server) {
+				  // console.log('serverChosen',server)
+			  },
+			  downloadStart: (data) => {
+				  // console.log('downloadStart',data)
+			  },
+			  downloadMeasurement: (data) => {
+				if(data?.Source === 'client'){
+					// console.log('downloadMeasurement',data)
+					console.log(data?.Data?.MeanClientMbps?.toFixed(2))
+				}
+			  },
+			  downloadComplete: (data) => {
+				  // console.log('downloadComplete',data)
+			  },
+			  uploadStart: (data) => {
+				   // console.log('uploadStart',data)
+			  },
+			  uploadMeasurement: function(data) {
+				  if(data.Source === 'client'){
+					  // console.log('uploadMeasurement',data)
+            console.log(data?.Data?.MeanClientMbps?.toFixed(2))
+				  }
+			  },
+			  uploadComplete: function (data) {
+				  // console.log('uploadComplete',data)
+			  },
+			},
+		).then(() => {}) // exit
+
+
+
+
